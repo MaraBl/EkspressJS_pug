bcecee032fca49dd0e3e5624222034fad2633944
Marta Blaszczak <marta.blaszczak1@gmail.com>
2019-08-12 22:24:28 +0100

    Add server.js main-index.pug and, login google.pug

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.use(express.static('views'));
+
+app.get('/', function (req, res) {
+    res.render('main-index', {
+    	first_name: req.query.first_name,
+    	last_name: req.query.last_name
+    });
+});
+
+app.get('/auth/google', function(req, res){
+    res.render('login-google');
+});
+
+var server = app.listen(3000, "localhost", function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+});
