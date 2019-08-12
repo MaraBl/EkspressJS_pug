147cd34250188e1aadf446f7ecb61d96fa913835
Marta Blaszczak <marta.blaszczak1@gmail.com>
2019-08-12 22:27:47 +0100

    improve formating

--- server.js
+++ server.js
@@ -7,18 +7,17 @@ app.set('views','./views');
 app.use(express.static('views'));
 
 app.get('/', function (req, res) {
-    res.render('main-index', {
-    	first_name: req.query.first_name,
-    	last_name: req.query.last_name
-    });
+	res.render('main-index', {
+		first_name: req.query.first_name,
+		last_name: req.query.last_name
+	});
 });
 
 app.get('/auth/google', function(req, res){
-    res.render('login-google');
+	res.render('login-google');
 });
 
 var server = app.listen(3000, "localhost", function() {
-  var host = server.address().address;
-  var port = server.address().port;
-
+	var host = server.address().address;
+	var port = server.address().port;
 });
